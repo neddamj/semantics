3ecf90be5b8e6265ef634f83961ba624657d9f06
neddamj <jordanmadden285@gmail.com>
2025-07-22 22:46:07 -0400

    implement semantic comm pipeline

--- pipeline.py
+++ pipeline.py
@@ -1,3 +1,21 @@
-# Will implement the full semantic communication pipeline.
-# Will give encoder, channel and decocer to the pipeline and it will 
-# return the output of the decoder.
+from typing import Protocol
+import torch.nn as nn
+
+class _Encoder(Protocol):
+    def forward(self, x): ...
+
+class _Decoder(Protocol):
+    def forward(self, z): ...
+
+class _Channel(Protocol):
+    def forward(self, z): ...
+
+class Pipeline(nn.Module):
+    def __init__(self, encoder: _Encoder, channel: _Channel, decoder: _Decoder):
+        super().__init__()
+        self.encoder, self.channel, self.decoder = encoder, channel, decoder
+
+    def forward(self, x):
+        z = self.encoder(x)
+        z_noisy = self.channel(z)
+        return self.decoder(z_noisy)
